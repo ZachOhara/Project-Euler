b585945e52143bc7c17574defce5db3edcd520e3
Zach Ohara <zjohara@gmail.com>
2014-12-08 23:15:46 -0800

    Updated RationalNumber some

--- src/common/RationalNumber.java
+++ src/common/RationalNumber.java
@@ -1,38 +1,54 @@
 package common;
 
-import java.util.ArrayList;
-
-import common.OharaMath;
-
-public class RationalNumber implements Comparable<RationalNumber> {
+public class RationalNumber implements Comparable<Object> {
 	
 	private int num;
 	private int den;
 	
+	// Constructors:
+	// Construct a new RationalNumber from a given numerator and denominator
 	public RationalNumber(int n, int d) {
 		if (d == 0)
 			throw new IllegalArgumentException();
 		this.num = n;
 		this.den = d;
+		this.simplify();
 	}
 	
+	// Construct a new RationalNumber with the same value as the given RationalNumber
+	public RationalNumber(RationalNumber r) {
+		this(r.num, r.den);
+	}
+	
+	// Construct a new RationalNumber with the value of the given integer
+	public RationalNumber(int n) {
+		this(n, 1);
+	}
+	
+	// Construct a new RationalNumber with the value of zero
 	public RationalNumber() {
 		this(0, 1);
 	}
 	
+	// Getters:
+	// Returns the numerator of this RationalNumber
 	public int getNumerator() {
 		return this.num;
 	}
 	
+	// Returns the denominator of this RationalNumber
 	public int getDenominator() {
 		return this.den;
 	}
 	
+	// Returns the approximate decimal value of this RationalNumber
 	public double decimalValue() {
 		return (double)this.num / this.den;
 	}
 	
-	public RationalNumber simplify() {
+	// Other Methods:
+	// Simplify the rational number
+	private RationalNumber simplify() {
 		if (this.num == 0)
 			this.den = 1;
 		int gcd = OharaMath.gcd(this.num, this.den);
@@ -41,44 +57,45 @@ public class RationalNumber implements Comparable<RationalNumber> {
 		return this;
 	}
 	
-	public RationalNumber negative(RationalNumber rn) {
-		return new RationalNumber(-rn.num, rn.den);
+	public RationalNumber negate() {
+		this.num *= -1;
+		this.simplify();
+		return this;
 	}
 	
-	public RationalNumber add(RationalNumber other) {
-		int num = (this.num * other.den) + (other.num * this.den);
-		int den = this.den * other.den;
-		return new RationalNumber(num, den);
+	public RationalNumber add(RationalNumber r) {
+		int num = (this.num * r.den) + (r.num * this.den);
+		int den = this.den * r.den;
+		return new RationalNumber(num, den).simplify();
 	}
 	
-	public RationalNumber subtract(RationalNumber other) {
-		return this.add(negative(other));
+	public RationalNumber subtract(RationalNumber r) {
+		return this.add(new RationalNumber(r).negate());
 	}
 	
-	public RationalNumber addSimplify(RationalNumber other) {
-		return this.add(other);
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		return this.compareTo(obj) == 0;
 	}
 	
-	public RationalNumber subtractSimplify(RationalNumber other) {
-		return subtract(other).simplify();
-	}
-	
-	public RationalNumber copyData() {
-		return new RationalNumber(this.num, this.den);
-	}
-	
-	public boolean isContained(RationalNumber[] rnarray) {
-		for (int i = 0; i < rnarray.length; i++)
-			if (this.equals(rnarray[i]))
-				return true;
-		return false;
-	}
-	
-	public boolean isContained(ArrayList<RationalNumber> rnarray) {
-		for (int i = 0; i < rnarray.size(); i++)
-			if (this.equals(rnarray.get(i)))
-				return true;
-		return false;
+	public int compareTo(Object obj) {
+		double thisval = this.decimalValue();
+		double otherval = 0;
+		if (obj instanceof RationalNumber)
+			otherval = ((RationalNumber) obj).decimalValue();
+		else if (obj instanceof Integer)
+			otherval = new Double((Integer)obj);
+		else if (obj instanceof Double)
+			otherval = (Double)(obj);
+		else
+			throw new ClassCastException();
+		
+		if (thisval < otherval)
+			return -1;
+		else if (otherval < thisval)
+			return 1;
+		return 0;
 	}
 	
 	public String toString() {
@@ -86,26 +103,5 @@ public class RationalNumber implements Comparable<RationalNumber> {
 			return "" + this.num;
 		else
 			return this.num + "/" + this.den;
-	}
-	
-	public boolean equals(RationalNumber other) {
-		/*
-		RationalNumber simplifyThis = this.copyData().simplify();
-		RationalNumber simplifyOther = other.copyData().simplify();
-		return (simplifyThis.num == simplifyOther.num && simplifyThis.den == simplifyOther.den);
-		*/
-		if (other == null)
-			return false;
-		return this.compareTo(other) == 0;
-	}
-	
-	public int compareTo(RationalNumber other) {
-		double thisdecimal = this.decimalValue();
-		double otherdecimal = other.decimalValue();
-		if (thisdecimal < otherdecimal)
-			return -1;
-		else if (otherdecimal < thisdecimal)
-			return 1;
-		return 0;
 	}
 }

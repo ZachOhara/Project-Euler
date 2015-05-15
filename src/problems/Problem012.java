5380e11160e4d1d23d14e83539fd233d2f615798
Zach Ohara <zjohara@gmail.com>
2015-05-14 19:32:13 -0700

    Solved Problem 12

--- src/problems/Problem012.java
+++ src/problems/Problem012.java
@@ -2,6 +2,7 @@ package problems;
 
 public class Problem012 {
 
+	/*
 	public static void main(String[] args) {
 		int num = 0;
 		int tri = 0;
@@ -20,14 +21,33 @@ public class Problem012 {
 		}
 		return sum;
 	}
+	*/
 	
-	public static int factors(int n) {
-		int factors = 0;
-		for (int i = 1; i <= n; i++) {
-			if (n % i == 0)
-				factors++;
+//	public static void main(String[] args) {
+//		int[] testcases = {1, 3, 6, 10, 15, 21, 28};
+//		for (int i : testcases)
+//			System.out.println(i + " : " + countFactors(i));
+//	}
+	
+	public static void main(String[] args) {
+		int cumul = 1;
+		int curr = 1;
+		while (countFactors(cumul) <= 500) {
+			curr++;
+			cumul += curr;
+			if (curr % 100 == 0)
+				System.out.println(curr + ", " + cumul + ", " + countFactors(cumul));
 		}
-		return factors;
+		System.out.println(cumul);
+	}
+	
+	public static int countFactors(int n) {
+		n = Math.abs(n);
+		int count = 0;
+		for (int i = 1; i <= n; i++)
+			if (n % i == 0)
+				count++;
+		return count;
 	}
 
 }

8b936eecadada58ac21a524549b361d7dc418b36
Zach Ohara <zjohara@gmail.com>
2015-05-14 20:50:18 -0700

    Problem 19

--- src/problems/Problem019.java
+++ src/problems/Problem019.java
@@ -1,16 +1,63 @@
 package problems;
 
 public class Problem019 {
-	
-	public static char[] DAYS_OF_WEEK = {'m', 't', 'w', 'h', 'f', 's', 'u'};
-	//h is tHurs; u is sUn. First letter of both names already taken...
-	public static int[] DAYS_IN_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	private static int weekday = 1; // 0: Sun, 1: Mon, ... , 6: Sat
+	private static int year = 1900;
+	private static int month = 1;
+	private static int day = 1;
 
 	public static void main(String[] args) {
-		//int totalsundays = 0;
-		for (int y = 1901; y <= 2000; y++) {
-			//if (y%4 == 0 && (!(y%100)))
+		int firstSundays = 0;
+		while (year <= 2000) {
+			if (year >= 1901 && day == 1 && weekday == 0)
+				firstSundays++;
+			progressDay();
+		}
+		System.out.println(firstSundays);
+	}
+	
+	public static void progressDay() {
+		weekday++;
+		day++;
+		if (weekday > 6) {
+			weekday = 0;
+		}
+		if (day > daysInMonth(month, year)) {
+			day = 1;
+			progressMonth();
 		}
 	}
 	
+	public static void progressMonth() {
+		month++;
+		if (month > 12) {
+			month = 1;
+			year++;
+		}
+	}
+
+	public static int daysInMonth(int month, int year) {
+		switch(month) {
+		case 9: // September
+		case 4: // April
+		case 6: // June
+		case 11: // November
+			return 30;
+		case 2: // February
+			return isLeapYear(year) ? 29 : 28;
+		default:
+			return 31;
+		}
+	}
+
+	public static boolean isLeapYear(int year) {
+		if (year % 4 == 0) {
+			if (year % 100 == 0 && !(year % 400 ==0))
+				return false;
+			return true;
+		} else
+			return false;
+	}
+
 }

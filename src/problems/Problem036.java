40e0bb53f403b6b51fb2756330984084cdb75ce7
Zach Ohara <zjohara@gmail.com>
2015-05-15 18:08:26 -0700

    Problem 36

---
+++ src/problems/Problem036.java
@@ -0,0 +1,20 @@
+package problems;
+
+import common.MathUtil;
+
+public class Problem036 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 0; i < 1000000; i++)
+			if (isFullPalindrome(i))
+				sum += i;
+		System.out.println(sum);
+	}
+	
+	public static boolean isFullPalindrome(int n) {
+		return MathUtil.isPalindromic("" + n)
+				&& MathUtil.isPalindromic(Integer.toBinaryString(n));
+	}
+
+}

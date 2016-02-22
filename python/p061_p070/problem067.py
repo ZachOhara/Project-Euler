31acfa3b66e7d10263e79bbf1cfddc841b9a74d5
Zach Ohara <zjohara@gmail.com>
2016-02-21 23:07:19 -0800

    Rename folders to be python-importable

--- python/p061_p070/problem067.py
+++ python/p061_p070/problem067.py
@@ -1,11 +1,11 @@
 from common import fileio
 
-import problem018
+from p011_p020 import problem018
 
 # This problem uses the same code as problem 18
 
 def main():
-	fileio.FILE_ADDRESS = "../res/problem067/triangle.txt"
+	fileio.FILE_ADDRESS = "../../res/problem067/triangle.txt"
 	print(problem018.getMaxSum(fileio.readFileAsMatrix()))
 
 if __name__ == "__main__":
